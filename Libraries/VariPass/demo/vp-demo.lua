a7dfba959d194b0fd49ccbce8e5d910c669d8823
Thorinair <thorinair@yahoo.com>
2017-02-12 20:06:21 +0100

    Updated the vp demo.

--- Libraries/VariPass/demo/vp-demo.lua
+++ Libraries/VariPass/demo/vp-demo.lua
@@ -1,7 +1,7 @@
 --[[
     Demo: VariPass Demo (vp-demo)
     Programmed by: Thorinair
-    Version: v1.0.0
+    Version: v1.0.1
     Description: Demo for testing the VariPass library.
     Usage:
     	Install the library, connect a Lever and a Redstone Lamp to a Redstone I/O.
@@ -12,7 +12,7 @@
     	The OpenComputers computer needs to have an Internet Card installed.
     	A Redstone I/O should be attached along with a Lever and a Redstone Lamp.
     	You need to have an account on VariPass (varipass.org).
-    	Additionally, the OCL Text library from this same repository should be installed on the OpenComputer.
+    	Additionally, the VariPass library from this same repository should be installed on the OpenComputer.
 --]]
 
 local vp = require('vp')
@@ -22,11 +22,11 @@ local redstone = component.redstone
 local sides = require('sides')
 
 -- Adjust these as needed.
-local key			= ""
-local id_lamp 		= ""
-local id_lever 		= ""
-local side_lamp 	= sides.top
-local side_lever 	= sides.south
+local key           = ""
+local id_lamp       = ""
+local id_lever 	    = ""
+local side_lamp     = sides.top
+local side_lever    = sides.south
 
 local sleep = 1
 
@@ -41,12 +41,22 @@ function process()
 				redstone.setOutput(side_lamp, 0)
 			end
 		end
+		if result == vp.RESULT_SUCCESS then
+			print("Success reading from VariPass!")
+		else 
+			print("Error reading from VariPass, code: " .. result)
+		end	
 
 		local active = false
 		if redstone.getInput(side_lever) > 0 then
 			active = true
 		end
 		local result = vp.write(key, id_lever, active)
+		if result == vp.RESULT_SUCCESS then
+			print("Success writing to VariPass!")
+		else 
+			print("Error writing to VariPass, code: " .. result)
+		end	
 
 		os.sleep(sleep)
 	end
